4e01e61e036709843bac4b46e940266ea6c2c367
wangjiangtao <13402947428@163.com>
2023-02-15 22:43:48 +0800

    first commit

---
+++ src/main/java/com/paul/petlove/PetLoveApplication.java
@@ -0,0 +1,13 @@
+package com.paul.petlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetLoveApplication.class, args);
+    }
+
+}
